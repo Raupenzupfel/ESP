587e36e4e4368a977707ac54ce541542f3cfae88
Tadeus <tadeus.ranisch@gmail.com>
2024-11-29 14:03:51 +0100

    Be- & Entlade verläufe

---
+++ MHS/python/Beladung.py
@@ -0,0 +1,46 @@
+# Beladungskurve Plotten
+# Johannes Tadeus Ranisch
+# 29.11.2024
+
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib.ticker import (MultipleLocator)
+
+## Import
+Ladung = pd.read_excel("C:/Users/tadeu/Documents/0 Studium/Master/1. Semester/ESP-PÜ/Git/MHS/python/RawData/geasamt.xlsx", usecols='A:D',sheet_name='Ladung')
+
+## Plotten
+fig, ax = plt.subplots()
+ax2 = ax.twinx()
+
+ax.set_xlabel('Zeit in Minuten')
+ax.set_ylabel('Druck in bar')
+ax.set_title('Beladungsverlauf')
+ax2.set_ylabel('Temperatur in °C')
+
+ax.xaxis.set_minor_locator(MultipleLocator(0.5))
+ax.grid()
+
+
+ax.plot(Ladung['t_m'],Ladung['p'], c = 'darkgreen', label='Druck')
+ax2.plot(Ladung['t_m'],Ladung['T'], c='dodgerblue', label='Temperatur')
+
+ax.axvline(5, c='orange')
+ax.axvline(13, c='orange')
+ax.axvline(21.5, c='orange')
+
+ax.text(0.5, 20, r'$\dot{V}=5\frac{Nl}{min}$')
+ax.text(7, 20, r'$\dot{V}=15\frac{Nl}{min}$')
+ax.text(17.5, 19, 'Abkühlen\nohne\nBeladung', ha='center')
+ax.text(22, 20, r'Eisbad mit $\dot{V}=15\frac{Nl}{min}$')
+
+ax.set_xlim(0, 33)
+ax.set_ylim(0, 22.5)
+
+fig.legend(bbox_to_anchor=(0.55, 0.3))
+
+plt.show()
+
+## Output
+fig.savefig('Beladungskurve.svg', bbox_inches = 'tight')
+fig.savefig('Beladungskurve.png', bbox_inches = 'tight', dpi=500)
---
+++ MHS/python/Entladung.py
@@ -0,0 +1,46 @@
+# Entladungskurve Plotten
+# Johannes Tadeus Ranisch
+# 29.11.2024
+
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib.ticker import (MultipleLocator)
+
+## Import
+Entladung = pd.read_excel("C:/Users/tadeu/Documents/0 Studium/Master/1. Semester/ESP-PÜ/Git/MHS/python/RawData/geasamt.xlsx", usecols='A:D',sheet_name='Entladung')
+
+## Plotten
+fig, ax = plt.subplots()
+ax2 = ax.twinx()
+
+ax.set_xlabel('Zeit in Minuten')
+ax.set_ylabel('Druck in bar')
+ax.set_title('Beladungsverlauf')
+ax2.set_ylabel('Temperatur in °C')
+
+ax.xaxis.set_minor_locator(MultipleLocator(0.5))
+ax.grid()
+
+
+ax.plot(Entladung['t_m'],Entladung['p'], c = 'darkgreen', label='Druck')
+ax2.plot(Entladung['t_m'],Entladung['T'], c='dodgerblue', label='Temperatur')
+
+ax.axvline(5.5, c='orange')
+ax.axvline(11, c='orange')
+ax.axvline(16.5, c='orange')
+
+ax.text(0.5, 12.5, r'$\dot{V}=5\frac{Nl}{min}$')
+ax.text(6, 12.5, r'$\dot{V}=11\frac{Nl}{min}$')
+ax.text(14, 3, 'Erwärmen\nohne\nBeladung', ha='center')
+ax.text(17, 4, r'Heißwasserbad mit $\dot{V}=11\frac{Nl}{min}$')
+
+ax.set_xlim(0, 29)
+ax.set_ylim(0, 22.5)
+
+fig.legend(bbox_to_anchor=(0.35, 0.88))
+
+plt.show()
+
+## Output
+fig.savefig('Entladungskurve.svg', bbox_inches = 'tight')
+fig.savefig('Entladungskurve.png', bbox_inches = 'tight', dpi=500)
