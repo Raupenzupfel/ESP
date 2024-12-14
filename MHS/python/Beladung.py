40f0c8b5d667f6220dd0ae049e42f790c56fb43a
Tadeus <tadeus.ranisch@gmail.com>
2024-12-14 12:14:43 +0100

    MHS Grafiken aktualisiert

--- MHS/python/Beladung.py
+++ MHS/python/Beladung.py
@@ -7,37 +7,62 @@ import matplotlib.pyplot as plt
 from matplotlib.ticker import (MultipleLocator)
 
 ## Import
-Ladung = pd.read_excel("C:/Users/tadeu/Documents/0 Studium/Master/1. Semester/ESP-PÜ/Git/MHS/python/RawData/geasamt.xlsx", usecols='A:D',sheet_name='Ladung')
+Ladung = pd.read_excel("C:/Users/tadeu/Documents/0 Studium/Master/1. Semester/ESP-PÜ/Git/MHS/python/RawData/geasamt.xlsx", usecols='A:F',sheet_name='Ladung')
 
+Speicheer_Leer = 6.489
+Masse = Ladung[Ladung['m'].notnull()]
 ## Plotten
 fig, ax = plt.subplots()
-ax2 = ax.twinx()
+ax2, ax3, ax4 = ax.twinx(), ax.twinx(), ax.twinx()
+
+fig.subplots_adjust(left=0.001)
+
+ax3.yaxis.set_ticks_position('left')
+ax3.yaxis.set_label_position('left')
+ax3.spines['left'].set_position(('axes', -0.12))
+
+ax4.set_axis_off()
+
+# ax4.yaxis.set_ticks_position('right')
+# ax4.yaxis.set_label_position('right')
+# ax4.spines['right'].set_position(('axes', 1.15))
 
 ax.set_xlabel('Zeit in Minuten')
 ax.set_ylabel('Druck in bar')
 ax.set_title('Beladungsverlauf')
 ax2.set_ylabel('Temperatur in °C')
+ax3.set_ylabel(r'Volumenstrom in $\frac{Nl}{min}$')
 
 ax.xaxis.set_minor_locator(MultipleLocator(0.5))
 ax.grid()
 
-
 ax.plot(Ladung['t_m'],Ladung['p'], c = '#76B900', label='Druck')
 ax2.plot(Ladung['t_m'],Ladung['T'], c='#0082D1', label='Temperatur')
+ax3.plot(Ladung['t_m'],Ladung['V'], c='#FF5F00', label='Volumenstrom')
+ax4.scatter(Ladung['t_m'],Ladung['m'], c='Black', label=r'$H_2$ Masse')
 
-ax.axvline(5, c='#FF5F00')
-ax.axvline(13, c='#FF5F00')
-ax.axvline(21.5, c='#FF5F00')
+ax.axvline(0, c='Black', ls=':')
+ax.axvline(5, c='Black', ls=':')
+ax.axvline(13, c='Black', ls=':')
+ax.axvline(21.5, c='Black', ls=':')
+ax.axvline(33, c='Black', ls=':')
 
-ax.text(0.5, 20, r'$\dot{V}=5\frac{Nl}{min}$')
-ax.text(7, 20, r'$\dot{V}=15\frac{Nl}{min}$')
+# ax.text(0.5, 20, r'$\dot{V}=5\frac{Nl}{min}$')
+# ax.text(7, 20, r'$\dot{V}=15\frac{Nl}{min}$')
 ax.text(17.5, 19, 'Abkühlen\nohne\nBeladung', ha='center')
-ax.text(22, 20, r'Eisbad mit $\dot{V}=15\frac{Nl}{min}$')
+for i, txt in enumerate(Ladung['m']):
+    txt = (Ladung['m'][i]-Speicheer_Leer)*1000
+    txt_formatted = "{:.1f}".format(txt)
+    ax4.annotate(txt_formatted + ' g', (Ladung['t_m'][i]+0.5, Ladung['m'][i]+0.00045))
 
-ax.set_xlim(0, 33)
+# ax.text(22, 20, r'Eisbad mit $\dot{V}=15\frac{Nl}{min}$')
+
+ax.set_xlim(-1, 36.5)
 ax.set_ylim(0, 22.5)
+ax3.set_ylim(0, 17.5)
+ax4.set_ylim(6.485, 6.525)
 
-fig.legend(bbox_to_anchor=(0.55, 0.3))
+fig.legend(bbox_to_anchor=(0.52, 0.35))
 
 plt.show()
 
--- MHS/python/Entladung.py
+++ MHS/python/Entladung.py
@@ -7,37 +7,58 @@ import matplotlib.pyplot as plt
 from matplotlib.ticker import (MultipleLocator)
 
 ## Import
-Entladung = pd.read_excel("C:/Users/tadeu/Documents/0 Studium/Master/1. Semester/ESP-PÜ/Git/MHS/python/RawData/geasamt.xlsx", usecols='A:D',sheet_name='Entladung')
+Entladung = pd.read_excel("C:/Users/tadeu/Documents/0 Studium/Master/1. Semester/ESP-PÜ/Git/MHS/python/RawData/geasamt.xlsx", usecols='A:F',sheet_name='Entladung')
+
+Speicher_Leer = 6.489
+Masse = Entladung[Entladung['m'].notnull()]
+print(Masse)
 
 ## Plotten
 fig, ax = plt.subplots()
-ax2 = ax.twinx()
+ax2, ax3, ax4 = ax.twinx(), ax.twinx(), ax.twinx()
+
+fig.subplots_adjust(left=0.001)
+
+ax3.yaxis.set_ticks_position('left')
+ax3.yaxis.set_label_position('left')
+ax3.spines['left'].set_position(('axes', -0.12))
+
+ax4.set_axis_off()
 
 ax.set_xlabel('Zeit in Minuten')
 ax.set_ylabel('Druck in bar')
 ax.set_title('Entladungsverlauf')
 ax2.set_ylabel('Temperatur in °C')
+ax3.set_ylabel(r'Volumenstrom in $\frac{Nl}{min}$')
 
 ax.xaxis.set_minor_locator(MultipleLocator(0.5))
 ax.grid()
 
-
 ax.plot(Entladung['t_m'],Entladung['p'], c = '#76B900', label='Druck')
 ax2.plot(Entladung['t_m'],Entladung['T'], c='#0082D1', label='Temperatur')
+ax3.plot(Entladung['t_m'],Entladung['V'], c='#FF5F00', label='Volumenstrom')
+ax4.scatter(Masse['t_m'], Masse['m'], c='Black', label=r'$H_2$ Masse')
 
-ax.axvline(5.5, c='#FF5F00')
-ax.axvline(11, c='#FF5F00')
-ax.axvline(16.5, c='#FF5F00')
+ax.axvline(0, c='Black', ls=':')
+ax.axvline(5.5, c='Black', ls=':')
+ax.axvline(11, c='Black', ls=':')
+ax.axvline(16.5, c='Black', ls=':')
+ax.axvline(28, c='Black', ls=':')
 
-ax.text(0.5, 12.5, r'$\dot{V}=5\frac{Nl}{min}$')
-ax.text(6, 12.5, r'$\dot{V}=11\frac{Nl}{min}$')
+# ax.text(0.5, 12.5, r'$\dot{V}=5\frac{Nl}{min}$')
+# ax.text(6, 12.5, r'$\dot{V}=11\frac{Nl}{min}$')
 ax.text(14, 3, 'Erwärmen\nohne\nBeladung', ha='center')
-ax.text(17, 4, r'Heißwasserbad mit $\dot{V}=11\frac{Nl}{min}$')
+for i, txt in enumerate(Entladung['m']):
+    txt = (Entladung['m'][i]-Speicher_Leer)*1000
+    txt_formatted = "{:.1f}".format(txt)
+    ax4.annotate(txt_formatted + ' g', (Entladung['t_m'][i]+0.5, Entladung['m'][i]-0.0003))
+# ax.text(17, 4, r'Heißwasserbad mit $\dot{V}=11\frac{Nl}{min}$')
 
-ax.set_xlim(0, 29)
+ax.set_xlim(-1, 30.5)
 ax.set_ylim(0, 22.5)
+ax3.set_ylim(0, 17.5)
 
-fig.legend(bbox_to_anchor=(0.35, 0.88))
+fig.legend(bbox_to_anchor=(0.8, 0.4))
 
 plt.show()
 
